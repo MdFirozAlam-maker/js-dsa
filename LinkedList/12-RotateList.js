e51d96ee709ace6f8beb4b838b1dea9a667c80a9
MdFirozAlam-maker <ifirozalam0786@gmail.com>
2026-05-16 00:09:02 +0530

    Solving RotateList and SwapPairsNodes

---
+++ LinkedList/12-RotateList.js
@@ -0,0 +1,30 @@
+function RotateList(head,k) {
+  if(!head || !head.next) return head;
+
+  //calculate length
+ let length = 0;
+ let curr = head;
+ while(curr) {
+  curr = curr.next;
+  length++;
+ }
+
+ k = k % length;
+
+  let slow = head;
+  let fast = head;
+  for(let i=0;i<k;i++) {
+    fast = fast.next;
+  }
+
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  fast.next = head;
+  let newhead = slow.next;
+  slow.next = null;
+
+  return newhead;
+}
