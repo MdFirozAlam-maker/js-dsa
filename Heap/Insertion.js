ca5f03c3938af463ba2522e81fd9f10712257aee
MdFirozAlam-maker <ifirozalam0786@gmail.com>
2026-07-16 17:41:50 +0530

    Adding insertion and extraction in heap

--- Heap/Insertion.js
+++ Heap/Insertion.js
@@ -1,5 +1,4 @@
 // Min Heap 
-
 class MinHeap {
   constructor () {
     //this.heap = [];
@@ -30,4 +29,46 @@ class MinHeap {
         tempIndex = this.getParentIndex(currIndex);
     }
   }
+
+  extract() {
+    if(this.heap.length < 1) return null;
+
+    let min = this.heap[0];
+    let lastIndex = this.heap.length - 1;
+
+
+    [this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]];
+
+    this.heap.pop();
+
+    this.heapifyDown(0);
+
+    return min;
+  }
+
+  heapifyDown(i) {
+    let left = this.getLeftChildIndex(i);
+    let right = this.getRightChildIndex(i);
+    let n = this.heap.length;
+
+    let smallest = i;
+    if(left < n && this.heap[left] < this.heap[smallest]) {
+      smallest = left;
+    }
+
+       if(right < n && this.heap[right] < this.heap[smallest]) {
+      smallest = right;
+    }
+
+    if(smallest != i) {
+      [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+      this.heapifyDown(smallest);
+    }
+  }
 }
+
+const heap = new MinHeap();
+heap.insert(1);
+console.log(heap.extract());
+
+
